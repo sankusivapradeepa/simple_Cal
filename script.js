ab5e7bcaca4283cba8a8a8c0f50c16dc3ce4de1a
sankusivapradeepa <sivapradeepa385@gmail.com>
2024-12-18 23:17:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function AddNum(){
+ let num1 = document.getElementById('firstbox').value
+ let num2 = document.getElementById('secondbox').value
+ num1 = parseInt(num1)
+ num2 = parseInt(num2)
+ alert(num1+num2);
+ }
+ function SubNum(){
+    let num1 = document.getElementById('firstbox').value
+    let num2 = document.getElementById('secondbox').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    alert(num1-num2);
+}
+function MulNum(){
+    let num1 = document.getElementById('firstbox').value
+    let num2 = document.getElementById('secondbox').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    alert(num1*num2);
+}
+function DivNum(){
+    let num1 = document.getElementById('firstbox').value
+    let num2 = document.getElementById('secondbox').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    alert(num1/num2);
+}
+function PowNum(){
+    let num1 = document.getElementById('firstbox').value
+    let num2 = document.getElementById('secondbox').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    alert(num1**num2);
+}
+
+
